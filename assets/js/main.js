a436fe29ef579c8ab4de85b972cc2152089acd98
mustafahmadov <mustafaia@code.edu.az>
2021-04-06 08:26:21 -0700

    about page finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,4 +77,25 @@ glowInTexts.forEach(glowInText => {
     glowInText.append(span);
   });
 });
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:true,
+          loop:false
+      }
+  }
+})
         
